c843556d180a128a5db96d3e9e72ae2b1932a884
pverg <peter.sv.1992@gmail.com>
2019-10-10 23:37:48 -0300

    login_screen
    Adicionei a tela de login e o primeiro esboço da tela principal uma vez que logado.

---
+++ app/src/main/java/br/com/fiap/penny/LoginActivity.java
@@ -0,0 +1,40 @@
+package br.com.fiap.penny;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText edtUsername;
+    EditText edtPassword;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        edtUsername = findViewById(R.id.edtUsername);
+        edtPassword = findViewById(R.id.edtPassword);
+    }
+
+    public void login(View view) {
+
+        if (edtUsername.getText().toString().equals("peter") && edtPassword.getText().toString().equals("123")) {
+
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+
+        } else {
+
+            Toast.makeText(this, "Usuário ou senha incorretos!", Toast.LENGTH_LONG);
+
+        }
+
+    }
+}
